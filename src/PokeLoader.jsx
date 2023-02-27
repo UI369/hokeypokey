b1fc0890ab22ce18400ae765ba3b4b99d7f88f34
UI369 <UI369x@gmail.com>
2023-02-27 07:57:10 -0800

    fixed unique key issue

--- src/PokeLoader.jsx
+++ src/PokeLoader.jsx
@@ -1,53 +1,49 @@
 import { useEffect, useState } from "react";
 
-export function PokeLoader({num}) {
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
+export function PokeLoader({ num }) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon/?limit=num&offset=0")
-          .then((res) => res.json())
-          .then(
-            (result) => {
-              setIsLoaded(true);
-              setItems(result);
-            },
-            (error) => {
-              setIsLoaded(true);
-              setError(error);
-            }
-          );
-      }, []);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=num&offset=0")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
 
-
-      if (error) {
-        return <div> Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return  <PokeList pokes={items.results}></PokeList>
-      }
+  if (error) {
+    return <div> Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return <PokeList pokes={items.results}></PokeList>;
+  }
 }
 
-function PokeList( {pokes} ){
-    let ndx = 0;
-    return pokes?.map((poke) => {
-        ndx++;
-        return <span>
-            <Pokemon name={poke.name} num={ndx} />
-        </span>
-    })
+function PokeList({ pokes }) {
+  return pokes?.map((poke, ndx) => {
+    //console.log(poke);
+    return <Pokemon key={poke.url} name={poke.name} num={ndx} />;
+  });
 }
-  
+
 function Pokemon({ name, num }) {
-return (
-    <div>Pokemon #{num}: {capitalizeFirstLetter(name)}</div>
-);
+  return (
+    <div>
+      Pokemon #{num}: {capitalizeFirstLetter(name)}
+    </div>
+  );
 
-
-function capitalizeFirstLetter(string) {
+  function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
+  }
 }
